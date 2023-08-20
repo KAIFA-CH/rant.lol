5710ea0598ea7d72ee0fad3779e0877b309d2e3f
Mar0xy <marie@kaifa.ch>
2023-08-20 02:13:18 +0200

    Swap out auth ui

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,11 +1,10 @@
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { Auth } from '@supabase/auth-ui-react';
+import { Auth } from '@maroxy/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 export default function AuthPage() {
     const supabase = useSupabaseClient();
 
-    return <Auth supabaseClient={supabase} providers={[]} magicLink={true} view='magic_link' appearance={{ theme: ThemeSupa }} />
+     return <Auth supabaseClient={supabase} providers={[]} magicLink={true} view='magic_link' appearance={{ theme: ThemeSupa }} />
 }
